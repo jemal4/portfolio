57e222966fafc89cef5a9035ef4be21bea6f0778
Natoli A <152323701+naatoli@users.noreply.github.com>
2024-05-05 08:43:16 +0000

    my update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Usmael Abdurhaman",
+  title: "Web Developer & Content Creator",
+  email: "uabdureman@gmail.com",
+  gitHub: "usmaelabdureman",
+  instagram: "esmizth",
+  linkedIn: "usmael-lkdn",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "Meetcode",
 };
 
 const primaryColor = "#4E567E";
